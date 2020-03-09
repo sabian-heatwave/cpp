43820e73273442065119cb4099362e7973511e12
Sabian Roberts <31491602+sabian-heatwave@users.noreply.github.com>
2020-03-09 12:47:17 +0000

    eggs
    I have no idea either

---
+++ cl_hw/Give Me Eggs.cpp
@@ -0,0 +1,11 @@
+// Copyright © 2020 Heatwave Studios
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string HelloILikeEggs = "please give me eggs...";
+    cout<<HelloILikeEggs;
+    return 0;
+}
